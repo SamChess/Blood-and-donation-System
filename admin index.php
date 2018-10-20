bfc815ab30de5b21480d4673fcc9c7eeed196df7
chinoyassin <38095481+chinoyassin@users.noreply.github.com>
2018-10-20 11:37:43 +0300

    Add files via upload

---
+++ admin index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel</title>
+<style>
+	body{
+		margin: 0px;
+		border: 0px;
+	}
+#header{
+	width: 100%;
+	height: 130px;
+	background: black;
+	color: white;
+}
+#sidebar{
+	width: 300px;
+	height: 400px;
+	background: #27ae60;
+	float: left;
+}
+#date{
+	height: 700px;
+	background: #c0392b;
+}
+ul li{
+	padding: 20px;
+}
+</style>
+</head>
+
+<body>
+<div id="header">
+	<center>This is the Admin's Module</center>
+</div>
+<div id="sidebar">
+	<ul>
+		<li> Add data</li>
+		<li>Delete data</li>
+		<li>Update data</li>
+		<li>Blood Bank</li>
+	</ul>
+</div>
+<div></div>
+</body>
+</html>
